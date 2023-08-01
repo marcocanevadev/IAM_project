c77c64812172a07f7731b03362d2468b21e3d8fb
marco caneva <marco.caneva.dev@outlook.com>
2023-08-01 15:19:58 +0200

    changed order

--- script6.m
+++ script6.m
@@ -1,58 +1,29 @@
-%   ---- Plot confusion matrix ----
+%   ---- Add Noise and perform Spectral Subtraction----
 
-%   ---- kNN 1, 2 and 3 ----
-figure(1)
-sgtitle('kNN 1, 2 and 3')
-subplot(2, 2, 1)
-C1 = confusionmat(gr_1_flag, predicted_label_1);
-cm1 = confusionchart(C1, {'MySongs'  'Electronic' 'Rap'},'OffDiagonalColor', "#7E2F8E", 'Title', ['Chroma    k = ',mat2str(k(b1))],'Normalization','column-normalized');
-subplot(2,2,2)
-C2 = confusionmat(gr_2_flag, predicted_label_2);
-cm2 = confusionchart(C2, {'MySongs'  'Electronic' 'Rap'},'OffDiagonalColor', "#7E2F8E", 'Title', ['MFCCs     k = ' mat2str(k(b2))],'Normalization', 'column-normalized');
-subplot(2,2,3)
-C3 = confusionmat(gr_3_flag, predicted_label_3);
-cm3 = confusionchart(C3, {'MySongs'  'Electronic' 'Rap'}, 'OffDiagonalColor', "#7E2F8E",'Title', ['All       k = ' mat2str(k(b3))],'Normalization','column-normalized');
-subplot(2,2,4)
 
-X = categorical({'CHROMA','MFCCS','ALL'});
-c = bar(X, rates,'FaceColor','flat');
-[~,indexbest] = max(rates);
-c.CData(indexbest+1,:)=[0.4940 0.1840 0.5560];
-xtips1 = c(1).XEndPoints;
-ytips1 = c(1).YEndPoints;
-labels1 = string(c(1).YData);
-text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
-    'VerticalAlignment','bottom')
-title('Rates')
-ylim([0,65])
-%   ---- Best kNN on noisy and reconstructed ----
 
-figure(2)
-sgtitle(['BEST kNN: Chroma (k = ', mat2str(k(b1)),')'])
-subplot(2,2,1)
-cm4 = confusionchart(C1, {'MySongs'  'Electronic' 'Rap'},'OffDiagonalColor', "#7E2F8E", 'Title', 'Clean','Normalization','column-normalized');
+[nn, nfs] = audioread('\data\babble.wav');
 
-subplot(2,2,2)
-C5 = confusionmat(best_dirty_truth, predicted_label_best_1);
-cm5 = confusionchart(C5, {'MySongs'  'Electronic' 'Rap'},'OffDiagonalColor', "#7E2F8E", 'Title', 'Noisy' ,'Normalization','column-normalized');
+la = noise('\data\A_mysongs\test\', 'wav' , nn, 5, [pwd '\data\noise_files\A\'] );
+lb = noise('\data\B_electronic\test\', 'mp3' , nn, 5, [pwd '\data\noise_files\B\'] );
+lc = noise('\data\C_rap\test\', 'mp3' , nn, 5, [pwd '\data\noise_files\C\']);
 
-subplot(2,2,3)
+for i = 1: length(la)
+    fname = la(i).name(1:end-4);
+    specname =  [ [pwd '\data\noise_spec_files\A\'], 'reconstructed_', fname, '.wav' ];
+    infile = [ [pwd '\data\noise_files\A\'],'noisy_', fname, '_5SNR.wav'];
+    specsub(infile,[pwd '\data\noise_spec_files\A\reconstructed_', fname,'_.wav'])
+end
 
-C6 = confusionmat(best_rec_truth, predicted_label_best_2);
-cm6 = confusionchart(C6, {'MySongs'  'Electronic' 'Rap'},'OffDiagonalColor', "#7E2F8E",'Title', 'Reconstructed' ,'Normalization','column-normalized');
-
-subplot(2, 2, 4)
-X2 = categorical({'Clean' 'Noisy' 'Reconstructed'});
-ratescat = [max(rates) rates_best];
-
-[~, indexbest2] = max(ratescat);
-
-c2 = bar(X2, ratescat, 'FaceColor','flat');
-ylim([0,65])
-xtips2 = c2(1).XEndPoints;
-ytips2 = c2(1).YEndPoints;
-labels2 = string(c2(1).YData);
-c2.CData(indexbest2,:)=[0.4940 0.1840 0.5560];
-text(xtips2,ytips2,labels2,'HorizontalAlignment','center',...
-    'VerticalAlignment','bottom')
-title('Rates')
+for i = 1: length(lb)
+    fname = lb(i).name(1:end-4);
+    specname =  [ [pwd '\data\noise_spec_files\B\'], 'reconstructed_', fname, '.wav' ];
+    infile = [ [pwd '\data\noise_files\B\'],'noisy_', fname, '_5SNR.wav'];
+    specsub(infile,[pwd '\data\noise_spec_files\B\reconstructed_', fname,'_.wav'])
+end
+for i = 1: length(lc)
+    fname = lc(i).name(1:end-4);
+    specname =  [ [pwd '\data\noise_spec_files\C\'], 'reconstructed_', fname, '.wav' ];
+    infile = [ [pwd '\data\noise_files\C\'],'noisy_', fname, '_5SNR.wav'];
+    specsub(infile,[pwd '\data\noise_spec_files\C\reconstructed_', fname,'_.wav'])
+end
--- script7.m
+++ script7.m
@@ -1,29 +1,58 @@
-%   ---- Add Noise and perform Spectral Subtraction----
+%   ---- Test kNN on noisy and recovered files ----
 
+winlen = 0.025;
+stp = winlen/3 *2;
+rates_best = zeros(1,2);
 
+%   ---- Noisy ----
 
-[nn, nfs] = audioread('\data\babble.wav');
+[A_best_dirty, ~] = chroma_mfcc_from_path('\data\noise_files\A\','wav',winlen,stp);
+[B_best_dirty, ~] = chroma_mfcc_from_path('\data\noise_files\B\','wav',winlen,stp);
+[C_best_dirty, ~] = chroma_mfcc_from_path('\data\noise_files\C\','wav',winlen,stp);
 
-la = noise('\data\A_mysongs\test\', 'wav' , nn, 5, [pwd '\data\noise_files\A\'] );
-lb = noise('\data\B_electronic\test\', 'mp3' , nn, 5, [pwd '\data\noise_files\B\'] );
-lc = noise('\data\C_rap\test\', 'mp3' , nn, 5, [pwd '\data\noise_files\C\']);
+A_best_dirty_flag = zeros(1,length(A_best_dirty));
+B_best_dirty_flag = ones(1, length(B_best_dirty));
+C_best_dirty_flag = repmat(2,1,length(C_best_dirty));
 
-for i = 1: length(la)
-    fname = la(i).name(1:end-4);
-    specname =  [ [pwd '\data\noise_spec_files\A\'], 'reconstructed_', fname, '.wav' ];
-    infile = [ [pwd '\data\noise_files\A\'],'noisy_', fname, '_5SNR.wav'];
-    specsub(infile,[pwd '\data\noise_spec_files\A\reconstructed_', fname,'_.wav'])
+best_dirty_truth = [A_best_dirty_flag B_best_dirty_flag C_best_dirty_flag];
+best_dirty_test = [A_best_dirty B_best_dirty C_best_dirty];
+best_dirty_test = normalize(best_dirty_test);
+best_dirty_test = best_dirty_test';
+
+predicted_label_best_1 = predict(bestMd, best_dirty_test);
+
+correct = 0;
+for i = 1:length(predicted_label_best_1)
+    if predicted_label_best_1(i) == best_dirty_truth(i)
+        correct = correct+1;
+    end
 end
+rates_best(1) = correct/length(predicted_label_best_1)*100;
 
-for i = 1: length(lb)
-    fname = lb(i).name(1:end-4);
-    specname =  [ [pwd '\data\noise_spec_files\B\'], 'reconstructed_', fname, '.wav' ];
-    infile = [ [pwd '\data\noise_files\B\'],'noisy_', fname, '_5SNR.wav'];
-    specsub(infile,[pwd '\data\noise_spec_files\B\reconstructed_', fname,'_.wav'])
+
+
+%   ---- Reconstructed ---- 
+
+[A_best_rec, ~] = chroma_mfcc_from_path('\data\noise_spec_files\A\','wav',winlen,stp);
+[B_best_rec, ~] = chroma_mfcc_from_path('\data\noise_spec_files\B\','wav',winlen,stp);
+[C_best_rec, ~] = chroma_mfcc_from_path('\data\noise_spec_files\C\','wav',winlen,stp);
+
+A_best_rec_flag = zeros(1,length(A_best_rec));
+B_best_rec_flag = ones(1, length(B_best_rec));
+C_best_rec_flag = repmat(2,1,length(C_best_rec));
+
+best_rec_truth = [A_best_rec_flag B_best_rec_flag C_best_rec_flag];
+best_rec_test = [A_best_rec B_best_rec C_best_rec];
+best_rec_test = normalize(best_rec_test);
+best_rec_test = best_rec_test';
+
+predicted_label_best_2 = predict(bestMd, best_rec_test);
+
+correcto = 0;
+for i = 1:length(predicted_label_best_2)
+    if predicted_label_best_2(i) == best_rec_truth(i)
+        correcto = correcto+1;
+    end
 end
-for i = 1: length(lc)
-    fname = lc(i).name(1:end-4);
-    specname =  [ [pwd '\data\noise_spec_files\C\'], 'reconstructed_', fname, '.wav' ];
-    infile = [ [pwd '\data\noise_files\C\'],'noisy_', fname, '_5SNR.wav'];
-    specsub(infile,[pwd '\data\noise_spec_files\C\reconstructed_', fname,'_.wav'])
-end
+rates_best(2) = correcto/length(predicted_label_best_2)*100;
+
